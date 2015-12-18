7af090d78592dd0c756a747b8a6c5b51294ae94c
Apostolis Konoupakis <konoupakis@gmail.com>
2015-12-18 02:58:31 +0200

    removed installer

--- scripts/install.js
+++
@@ -1,6 +0,0 @@
-var packInfo = require('../package');
-var installer = require('jsnbt/installer');
-
-console.log('installing ' + packInfo.name + ' v' + packInfo.version);
-installer.npm.pack(packInfo.name, true);
-console.log('installed ' + packInfo.name + ' v' + packInfo.version);
