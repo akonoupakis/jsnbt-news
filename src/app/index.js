98bf3ac733acdf05c50e40ef1bd5e5ed2a293abd
Apostolis Konoupakis <konoupakis@gmail.com>
2016-01-03 06:14:07 +0200

    modules fixes

--- src/app/index.js
+++ src/app/index.js
@@ -29,7 +29,7 @@ module.exports = {
 
     url: {
 
-        build: function (options, next) {
+        build: function (server, options, next) {
             if (options.node.entity === 'article') {
                 for (var item in options.url) {
                     options.url[item] += '/article?aid=' + options.node.id;
@@ -41,119 +41,132 @@ module.exports = {
             }            
         },
 
-        resolve: function (options, next) {
+        resolve: function (server, options, next) {
 
-            if (options.page) {
-                next(options);
-            }
-            else {
-                var lastNode = _.last(options.nodes);
-                var lastSeoName = _.last(options.seoNames);
-                
-                if ((lastSeoName || '').toLowerCase() === 'article') {
+            if (options.page)
+                return next(options);
 
-                    var uri = new parseUri(options.url);
+            var lastNode = _.last(options.nodes);
+            var lastSeoName = _.last(options.seoNames);
 
-                    var getArticle = function (parentId, articleId, cb) {
-                        options.db.nodes.getCached({
+            if ((lastSeoName || '').toLowerCase() === 'article') {
+
+                var uri = new parseUri(options.url);
+
+                var getArticle = function (parentId, articleId, cb) {
+                    var nodesStore = server.db.createStore('nodes');
+                    nodesStore.get(function (x) {
+                        x.query({
                             "id": articleId,
                             entity: 'article',
                             parent: parentId
-                        }, function (ex, article) {
-                            if (ex) {
-                                next();
-                            }
-                            else {
-                                cb(article);
+                        });
+                        x.single();
+                        x.cached();
+                    }, function (ex, article) {
+                        if (ex) 
+                            return cb(ex);
+                        
+                        cb(null, article);
+                    });
+                };
+
+                var patchTemplate = function (parent, options, cb) {
+                    if (options.page.template && options.page.template !== '') {
+                        options.template = options.page.template;
+                        cb(null, options);
+                    }
+                    else {
+
+                        var templ = undefined;
+
+                        _.each(options.nodes, function (rnode) {
+                            if (rnode.content.articleTemplate && !rnode.content.articleTemplate.inherits === true) {
+                                if (rnode.content.articleTemplate.value)
+                                    templ = rnode.content.articleTemplate.value;
                             }
                         });
-                    };
 
-                    var patchTemplate = function (parent, options, cb) {
-                        if (options.page.template && options.page.template !== '') {
-                            options.template = options.page.template;
-                            cb(options);
+                        if (templ) {
+                            options.template = templ;
+                            cb(null, options);
                         }
                         else {
-
-                            var templ = undefined;
-
-                            _.each(options.nodes, function (rnode) {
-                                if (rnode.content.articleTemplate && !rnode.content.articleTemplate.inherits === true) {
-                                    if (rnode.content.articleTemplate.value)
-                                        templ = rnode.content.articleTemplate.value;
-                                }
-                            });
-
-                            if (templ) {
-                                options.template = templ;
-                                cb(options);
-                            }
-                            else {
-                                options.db.settings.getCached({
+                            var settingsStore = server.db.createStore('settings');
+                            settingsStore.get(function (x) {
+                                x.query({
                                     domain: 'news'
-                                }, function (ex, settings) {
-                                    if (ex) {
-                                        cb(options);
-                                    }
-                                    else {
-                                        var set = _.first(settings);
-                                        if (set && _.isObject(set.data) && set.data.articleTemplate) {
-                                            options.template = set.data.articleTemplate;
-                                        }
-
-                                        cb(options);
-                                    }
                                 });
-                            }
+                                x.cached();
+                                x.single();
+                            }, function (ex, settings) {
+                                if (ex) 
+                                    return cb(ex);
+                                
+                                if (settings && _.isObject(settings.data) && settings.data.articleTemplate) {
+                                    options.template = settings.data.articleTemplate;
+                                }
+
+                                cb(options);
+                            });
                         }
-                    };
+                    }
+                };
 
-                    if (lastNode.domain === 'core' && lastNode.entity === 'pointer') {
+                if (lastNode.domain === 'core' && lastNode.entity === 'pointer') {
 
-                        options.db.nodes.getCached({
+                    var nodesStore = server.db.createStore('nodes');
+                    nodesStore.get(function (x) {
+                        x.query({
                             "id": lastNode.pointer.nodeId,
                             entity: 'articleList'
-                        }, function (ex, articleList) {
-                            if (ex) {
-                                next();
-                            }
-                            else {
-                                if (uri.queryKey.aid) {
-                                    getArticle(articleList.id, uri.queryKey.aid, function (article) {
-                                        options.page = article;
-                                        options.nodes.push(articleList);
-                                        options.nodes.push(article);
-                                        patchTemplate(articleList, options, function (options) {
-                                            next(options);
-                                        });
-                                    });
-                                }
-                                else {
-                                    next();
-                                }
-                            }
                         });
-
-                    }
-                    else if (lastNode.entity === 'articleList') {
-                        if (uri.queryKey.aid) {                            
-                            getArticle(lastNode.id, uri.queryKey.aid, function (article) {
-                                options.page = article;
-                                options.nodes.push(article);
-                                patchTemplate(lastNode, options, function (options) {
-                                    next(options);
-                                });
-                            });
+                        x.cached();
+                    }, function (ex, articleList) {
+                        if (ex) {
+                            return next();
                         }
                         else {
-                            next();
+                            if (uri.queryKey.aid) {
+                                getArticle(articleList.id, uri.queryKey.aid, function (artErr, article) {
+                                    if (artErr)
+                                        return next();
+
+                                    options.page = article;
+                                    options.nodes.push(articleList);
+                                    options.nodes.push(article);
+                                    patchTemplate(articleList, options, function (options) {
+                                        next(options);
+                                    });
+                                });
+                            }
+                            else {
+                                next();
+                            }
                         }
+                    });
+
+                }
+                else if (lastNode.entity === 'articleList') {
+                    if (uri.queryKey.aid) {
+                        getArticle(lastNode.id, uri.queryKey.aid, function (artErr, article) {
+                            if (artErr)
+                                return next();
+                            
+                            options.page = article;
+                            options.nodes.push(article);
+                            patchTemplate(lastNode, options, function (options) {
+                                next(options);
+                            });
+                        });
+                    }
+                    else {
+                        next();
                     }
                 }
-                else {
-                    next();
-                }
+            }
+            else {
+                next();
             }
         }
 
